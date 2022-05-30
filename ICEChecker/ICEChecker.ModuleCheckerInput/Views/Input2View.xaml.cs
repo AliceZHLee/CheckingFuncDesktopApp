375f2cb5e7accab42f596b6ea2b3a39317283fac
AliceZHLee <57344614+AliceZHLee@users.noreply.github.com>
2022-05-30 14:52:51 +0800

    Update Input2View.xaml.cs

--- ICEChecker/ICEChecker.ModuleCheckerInput/Views/Input2View.xaml.cs
+++ ICEChecker/ICEChecker.ModuleCheckerInput/Views/Input2View.xaml.cs
@@ -1,18 +1,11 @@
-﻿//using Microsoft.Office.Interop.Excel;
-using ICEChecker.ModuleCheckerInput.Models;
-using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.OleDb;
 using System.Windows.Controls;
 
 namespace ICEChecker.ModuleCheckerInput.Views {
     /// <summary>
     /// Interaction logic for InputView.xaml
     /// </summary>
-    public partial class Input2View : UserControl {
-        public Input2View() {
+    public partial class InputView : UserControl {
+        public InputView() {
             InitializeComponent();
         }
         private void BtnFilter_Click(object sender, System.Windows.RoutedEventArgs e) {
